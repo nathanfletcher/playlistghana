f8e5a40c95eb5bbcdd1ed2a5ac973c9e96a4ff52
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2013-06-24 16:41:41 +0000

    adding all files to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playlistghana::Application.config.secret_token = 'f995831576956f85bfa7ab67bd82211c660cc48d5298400ac34a10b1bd344521770c6c76c90a2e5a47f69d85c1948d025ccec697784d41f6a065b295077ac3bb'
